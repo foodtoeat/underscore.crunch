3d5ffcbedeee8de7fb8f2ff9f23feaa4d03fb2a1
ahamal <ashes09@gmail.com>
2014-12-11 18:53:03 -0500

    Updated readme documentation.

--- README.md
+++ README.md
@@ -14,18 +14,21 @@ crunched = _.crunch(<L>)
 
 Grammar: 
 ```
-<L> = [<l>, ..]
-<l> = function or { pre: <L>, post: <L> }
+<L> -> [<l>, ..]
+<l> -> <function> or { pre: <L>, post: <L> }
 ```
+* All functions on L and in 'pre' are executed parallelly,
+* All functions on 'post' are executed after 'pre'
 
-All functions on L and in 'pre' are executed parallelly,
-All functions on 'post' are executed after 'pre'
+eg. 
+```
+_.crunch([cb1, cb2, cb3, {pre:[cb4], post:[cb5, cb6] }])
+```
+`cb1`, `cb2`, `cb3`, `cb4` run parallelly,
 
-eg. [cb1, cb2, cb3, {pre:[cb4], post:[cb5, cb6] }]
-runs cb1, cb2, cb3, cb4 parallelly, 
-cb5, cb6 executes after cb4 is complete.
+`cb5`, `cb6` executes after `cb4` is complete.
 
-You'll need to use proxy for backbone models. eg. 
+**Note** If you're using it for backbone.js models, You'll need `$.proxy`. eg.
 ```javascript
 var crunched = _.crunch([
   $.proxy(model1.fetch, model1), 
--- src/underscore.plus.js
+++ src/underscore.plus.js
@@ -15,34 +15,7 @@
   }
 }(this, function(_) {
 
-  // Crunch combines multiple callback functions with
-  // {success:, error:, complete:} recursively into one function.
-  // 
-  // Syntax:
-  // crunched = _.crunch(<L>)
-  // 
-  // Grammar: 
-  // <L> = [<l>, ..]
-  // <l> = function or { pre: <L>, post: <L> }
-  // 
-  // All functions on L and in 'pre' are executed parallelly,
-  // All functions on 'post' are executed after 'pre'
-  // 
-  // eg. [cb1, cb2, cb3, {pre:[cb4], post:[cb5, cb6] }]
-  // runs cb1, cb2, cb3, cb4 parallelly, 
-  // cb5, cb6 executes after cb4 is complete.
-  // 
-  // You'll need to use proxy for backbone models.
-  // eg. 
-  // var crunched = _.crunch([
-  //   $.proxy(model1.fetch, model1), 
-  //   $.proxy(model2.fetch, model2)
-  // ])
-  // 
-  // crunched({
-  //   success: ...
-  // })
-
+  // Combines multiple callback functions  recursively into one function. 
   var crunch = _.crunch = function(calls) {
     return function(callbacks) {
       var _callbacks = {
