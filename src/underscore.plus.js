d147f3609ed18aed907c157501a26589dc0c32ef
ahamal <ashes09@gmail.com>
2015-01-22 21:59:02 -0500

    New more flexible grammer. Added recursively structured return for each calls.

--- src/underscore.plus.js
+++ src/underscore.plus.js
@@ -15,59 +15,69 @@
   }
 }(this, function(_) {
 
-  // Combines multiple callback functions  recursively into one function. 
-  var crunch = _.crunch = function(calls) {
-    return function(callbacks) {
-      var _callbacks = {
-        success: _.after(
-          calls.length,
-          _.isFunction(callbacks.success) ? callbacks.success : function() {}
-        ),
-        error: _.once(
-          _.isFunction(callbacks.error) ? callbacks.error : function() {}
-        ),
-        complete: _.after(
-          calls.length,
-          _.isFunction(callbacks.complete) ? callbacks.complete : function() {}
-        )
-      };
-
-      _.each(calls, function(c) {
-        if (_.isFunction(c)) {
-          c(_callbacks);
-        } else if (_.isArray(c.pre) && _.isArray(c.post)) {
-          _callbacks.complete = _.after(2, _callbacks.complete);
-          crunch(c.pre).call(undefined, _.extend({}, _callbacks, {
-              success: function() {
-                crunch(c.post).call(undefined, _callbacks);
-              },
-              error: function() {
-                _callbacks.error.apply(undefined, arguments);
-                _callbacks.complete();
-              },
-              complete: function() {
-                _callbacks.complete();
-              }
-            })
-          );
-        } else {
-          throw 'Invalid structure';
-        }
-      });
-    };
-  };
-
-  var complete = _.complete = function(callbacks) {
-    if (callbacks && _.isFunction(callbacks.complete) ) callbacks.complete();
-  };
-
+  // Calls callback's success method
   var succeed = _.succeed = function(callbacks) {
     if (callbacks && _.isFunction(callbacks.success) ) callbacks.success();
   };
 
+  // Calls callback's complete method
+  var complete = _.complete = function(callbacks) {
+    if (callbacks && _.isFunction(callbacks.complete) ) callbacks.complete();
+  };
+
+  // Calls callback's success and complete
   var finish = _.finish = function(callbacks) {
     complete(callbacks);
     succeed(callbacks);
+  };
+
+  // Combines multiple callback functions  recursively into one function.
+  var crunch = _.crunch = function(x, ignore_fail) {
+    return function(callbacks) {
+      var _callbacks;
+      if (_.isFunction(x)) return x(callbacks);
+
+      if (_.isArray(x) && !_.isEmpty(x)) {
+        _callbacks = {
+          success: _.after( x.length, callbacks.success || function() {} ),
+          error: _.once( callbacks.error || function() {} ),
+          complete: _.after( x.length, callbacks.complete || function() {} )
+        };
+
+        return _.map(x, function(_x) {
+          return crunch(_x, ignore_fail) (_callbacks);
+        });
+      }
+
+      if (x && x.pre && x.post) {
+        var result = {};
+
+        _callbacks = _.extend({}, {
+          complete: _.after(2, callbacks.complete)
+        }, callbacks);
+
+        result.pre = crunch(x.pre).call(undefined, {
+          success: function() {
+            result.post = crunch(x.post, ignore_fail).call(undefined, _callbacks);
+          },
+          error: function() {
+            _callbacks.error.apply(undefined, arguments);
+
+            if (ignore_fail){
+              result.post = crunch(x.post, ignore_fail).call(undefined, _callbacks);
+            } else {
+              _.complete(_callbacks);
+            } 
+          },
+          complete: function() {
+            _.complete(_callbacks);
+          }
+        });
+
+        return result;
+      }
+      throw 'Invalid structure for _.crunch';
+    };
   };
 
   return _;
